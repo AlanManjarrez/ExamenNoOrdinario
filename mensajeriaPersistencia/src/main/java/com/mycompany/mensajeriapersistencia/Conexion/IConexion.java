7e8fac31f3ff4ac0501ef1c426bc7d53b98d3d84
AlanManjarrez <160210523+AlanManjarrez@users.noreply.github.com>
2024-05-25 18:06:03 -0700

    Creacion IConexion

---
+++ mensajeriaPersistencia/src/main/java/com/mycompany/mensajeriapersistencia/Conexion/IConexion.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.mensajeriapersistencia.Conexion;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Interfaz para la creación de conexiones.
+ * 
+ * Esta interfaz define un método para crear una conexión a una base de datos.
+ * Implementaciones específicas proporcionarán los detalles para establecer 
+ * la conexión a diferentes tipos de bases de datos.
+ * 
+ * 
+ * @author Jose Alan Manjarrez Ontiveros 228982
+ */
+public interface IConexion {
+    
+    /**
+     * Crea una nueva conexión a la base de datos.
+     * 
+     * Este método debe ser implementado para establecer una conexión a una base 
+     * de datos específica y devolver un objeto {@code Connection} que represente 
+     * esa conexión.
+     * 
+     *
+     * @return una conexión a la base de datos
+     * @throws SQLException si ocurre un error al intentar establecer la conexión
+     */
+    public Connection crearConexion() throws SQLException;
+}
